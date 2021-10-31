a570b64bf1a215032dedf287031761b4c0d99ffd
devankit01 <devankitgupta01@gmail.com>
2021-10-31 11:38:49 +0530

    Front end 04 and template logic

--- SubApp/urls.py
+++ SubApp/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('auth/signup/' , signup , name = 'signup'),
+    path('auth/signin/' , signin , name = 'signin'),
     path('<slug>/' , article , name = 'article'),
 ]
--- SubApp/views.py
+++ SubApp/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status='Published')
     return render(request, 'index.html',{'posts' : posts})
 
 def article(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'article.html',{'post' : post})
+
+def signup(request):
+    return render(request, 'signup.html',)
+
+def signin(request):
+    return render(request, 'signin.html',)
